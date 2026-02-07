cb4b1dce646741a0e09decfbbb0292a0660604ca
Jeetroy792 <jeetroy5335@gmail.com>
2026-02-07 19:26:04 +0530

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,64 +1,35 @@
 import time as tm
-from database import db 
-from .test import parse_buttons
-
-STATUS = {}
+from database import db
 
 class STS:
     def __init__(self, id):
-        self.id = id
-        self.data = STATUS
-    
-    def verify(self):
-        return self.data.get(self.id)
-    
-    def store(self, From, to,  skip, limit):
-        self.data[self.id] = {"FROM": From, 'TO': to, 'total_files': 0, 'skip': skip, 'limit': limit,
-                      'fetched': skip, 'filtered': 0, 'deleted': 0, 'duplicate': 0, 'total': limit, 'start': 0}
-        self.get(full=True)
-        return STS(self.id)
-        
-    def get(self, value=None, full=False):
-        values = self.data.get(self.id)
-        if not full:
-           return values.get(value)
-        for k, v in values.items():
-            setattr(self, k, v)
-        return self
+        self.id = str(id)
 
-    def add(self, key=None, value=1, time=False):
-        if time:
-          return self.data[self.id].update({'start': tm.time()})
-        self.data[self.id].update({key: self.get(key) + value}) 
-    
-    def divide(self, no, by):
-       by = 1 if int(by) == 0 else by 
-       return int(no) / by 
-    
-    async def get_data(self, user_id, client=None):
-        bot = await db.get_bot(user_id)
-        k, filters = self, await db.get_filters(user_id)
-        size, configs = None, await db.get_configs(user_id)
-        
-        # Userbot Client logic integration
-        current_client = client.user if client and hasattr(client, 'user') else client
-        
-        if configs['duplicate']:
-           duplicate = [configs['db_uri'], self.TO]
-        else:
-           duplicate = False
-        button = parse_buttons(configs['button'] if configs['button'] else '')
-        if configs['file_size'] != 0:
-            size = [configs['file_size'], configs['size_limit']]
-            
-        return bot, configs['caption'], configs['forward_tag'], {
-                'chat_id': k.FROM, 
-                'limit': k.limit, 
-                'offset': k.skip, 
-                'filters': filters,
-                'keywords': configs['keywords'], 
-                'media_size': size, 
-                'extensions': configs['extension'], 
-                'skip_duplicate': duplicate,
-                'client': current_client # Passing userbot client for private access
-            }, configs['protect'], button
+    def verify(self):
+        # ডাটাবেস থেকে ডাটা চেক করা
+        return db.get_forward_data(self.id)
+
+    def store(self, From, to, skip, limit):
+        data = {
+            "_id": self.id,
+            "FROW": From, 
+            "TO": to, 
+            "total_files": 0, 
+            "skip": skip,
+            "fetched": skip, 
+            "filtered": 0, 
+            "deleted": 0, 
+            "duplicate": 0,
+            "last_msg_id": limit
+        }
+        db.save_forward_data(self.id, data)
+        return STS(self.id)
+
+    def get(self, value=None, full=False):
+        data = db.get_forward_data(self.id)
+        if not data:
+            return None
+        if not full:
+            # chat_id, toid, skip, last_msg_id ফরম্যাটে রিটার্ন
+            return data.get('FROW'), data.get('TO'), data.get('skip'), data.get('last_msg_id')
+        return data
